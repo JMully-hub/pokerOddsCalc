643c631c2a226db05b3de55d00b89556e7eef928
JMully-hub <54962030+JMully-hub@users.noreply.github.com>
2021-01-31 00:07:02 +0000

    Added changes to support single display

--- pokerOddsCalc.py
+++ pokerOddsCalc.py
@@ -4,6 +4,8 @@
 
 
 import sys, time, pyautogui, subprocess, os, logging, cv2
+import pygetwindow as gw
+from screeninfo import get_monitors
 import pyinputplus as pyip
 from pathlib import Path
 from selenium import webdriver
@@ -12,42 +14,42 @@ from selenium.common.exceptions import ElementNotInteractableException
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 from selenium.webdriver.firefox.options import Options
 
-logging.disable(logging.CRITICAL)
+#logging.disable(logging.CRITICAL)
 logging.basicConfig(level=logging.INFO, format='%(asctime)s -  %(levelname)s-  %(message)s')
 logging.info('Start of program')
 
 def activePlayers():
     otherPlayers = 0
     for location in playerSeats:
-        if pyautogui.locateOnScreen(str(Path('./images/cardBack.png')),
+        if pyautogui.locateOnScreen(str(Path('./images/'+screenKey+'cardBack.png')),
                                     region=(playerSeats[location]), confidence=0.99) is not None:
             otherPlayers = otherPlayers + 1
     return otherPlayers
 
 def findCard(dictionary, key):
-    deckList =  os.listdir(r'.\images\suits')
+    deckList =  os.listdir(Path('./images/'+screenKey+'suits'))
     returnDic = {}
     for s in deckList:
-        if pyautogui.locateOnScreen(('.\\images\\suits\\'+s),
+        if pyautogui.locateOnScreen(str(Path('./images/'+screenKey+'suits/'+s)),
                                     region=dictionary[key], confidence=0.99) is not None:
             suit = str(os.path.basename(s)).replace('.png', '')
             if suit == 'HEART' or suit == 'DIAMOND':
-                valueList = os.listdir(r'.\images\redValues')
+                valueList = os.listdir(str(Path('./images/'+screenKey+'redValues')))
                 for v in valueList:
-                    if pyautogui.locateOnScreen(('.\\images\\redValues\\'+v),
+                    if pyautogui.locateOnScreen(str(Path('./images/'+screenKey+'redValues/'+v)),
                                                 region=dictionary[key], confidence=0.99) is not None:
                         value = str(os.path.basename(v)).replace('.png', '')
-                        card = str(value+suit)
+                        card = value+suit
                         returnDic.setdefault('SUIT', suit)
                         returnDic.setdefault('CARD', card)
                         return returnDic
             elif suit == 'CLUB' or suit == 'SPADE':
-                valueList = os.listdir(r'.\images\blackValues')
+                valueList = os.listdir(str(Path('./images/'+screenKey+'blackValues')))
                 for v in valueList:
-                    if pyautogui.locateOnScreen(('.\\images\\blackValues\\'+v),
+                    if pyautogui.locateOnScreen(str(Path('./images/'+screenKey+'blackValues/'+v)),
                                                 region=dictionary[key], confidence=0.99) is not None:
                         value = str(os.path.basename(v)).replace('.png', '')
-                        card = str(value+suit)
+                        card = value+suit
                         returnDic.setdefault('SUIT', suit)
                         returnDic.setdefault('CARD', card)
                         return returnDic
@@ -146,9 +148,15 @@ def clickRiver():
     link.click()
     done.click()
 
+monitorList = []
+for m in get_monitors():
+	monitorList.append(m)
 
-print('Press CTRL + C to quit')
-sizeOfTable = pyip.inputMenu(['2', '6', '9'], 'Enter how many seats at table:\n')
+print('\nONLY WORKS WITH 1920x1080 DISPLAYS\n\nWorks best if you have the poker room\n'
+      'already open before starting this program\n\nPress CTRL + C to quit\n')
+if len(monitorList) > 1:
+    sameScreenOption = pyip.inputYesNo(prompt='Display the calculator on the same screen as the poker table? Y/N: ')
+sizeOfTable = pyip.inputMenu(['2', '6', '9'], '\nEnter how many seats at table:\n')
 if sizeOfTable == '2':
     mySeat = pyip.inputMenu(['1', '2'], 'Please select own seat number, 1 = top, 2 = bottom\n')
 elif sizeOfTable == '6':
@@ -156,15 +164,36 @@ elif sizeOfTable == '6':
 elif sizeOfTable == '9':
     mySeat = pyip.inputMenu(['1', '2', '3', '4', '5', '6', '7', '8', '9'], 'Please select own seat number 1-9 clockwise\n')
 
+
 options = Options()
 options.add_argument('--kiosk')
 binary = FirefoxBinary(str(Path(r'C:/Program Files/Mozilla Firefox/firefox.exe')))
 driver = webdriver.Firefox(options=options, firefox_binary = binary, executable_path = str(Path('.\geckodriver.exe')))
 driver2 = webdriver.Firefox(options=options, firefox_binary = binary, executable_path = str(Path('.\geckodriver.exe')))
 driver.set_window_size(466, 500)
-driver.set_window_position(1914,0)
 driver2.set_window_size(466, 550)
-driver2.set_window_position(1914,494)
+if len(monitorList) == 1 or sameScreenOption == 'yes':
+    if gw.getWindowsWithTitle('Hold\'em') != []:
+        pokerRoom = gw.getWindowsWithTitle('Hold\'em')[0]
+        pokerRoom.resizeTo(1480, 1048)
+        pokerRoom.moveTo(-9, 0)
+        pyautogui.keyDown('altleft')
+        pokerRoom.activate()
+        pyautogui.keyUp('altleft')
+    driver.set_window_position(1457,0)
+    driver2.set_window_position(1457,494)
+    screenKey = '1'
+elif len(monitorList) > 1 and sameScreenOption == 'no':
+    driver.set_window_position(1914,0)
+    driver2.set_window_position(1914,494)
+    screenKey = '2'
+    if gw.getWindowsWithTitle('Hold\'em') != []:
+        pokerRoom = gw.getWindowsWithTitle('Hold\'em')[0]
+        pokerRoom.maximize()
+        pyautogui.keyDown('altleft')
+        pokerRoom.activate()
+        pyautogui.keyUp('altleft')
+
 driver.get('https://www.888poker.com/poker/poker-odds-calculator')
 driver2.get('https://betandbeat.com/poker/rules/hands/')
 
@@ -225,51 +254,84 @@ deckDict = {'CLUB': driver.find_element_by_xpath('//*[@id="suit-selection-contai
             'kingHEART' : driver.find_element_by_xpath('//*[@id="card-selection-table"]/div[1]/div[4]/a[12]'),
             'aceHEART' : driver.find_element_by_xpath('//*[@id="card-selection-table"]/div[1]/div[4]/a[13]')}
 
-myHandPositions9 = {'1a': (1104,41,40,80), '1b': (1200,40,40,80),
-                   '2a': (1377,141,40,80), '2b': (1474,140,40,80),
-                   '3a': (1441,342,40,80), '3b': (1535,340,40,80),
-                   '4a': (1292,545,40,80), '4b': (1389,549,40,80),
-                   '5a': (864,640,40,80), '5b': (962,640,40,80),
-                   '6a': (436,548,40,80), '6b': (534,548,40,80),
-                   '7a': (297,342,40,80), '7b': (388,339,40,80),
-                   '8a': (351,137,40,80), '8b': (447,140,40,80),
-                   '9a': (627,43,40,80), '9b': (722,41,40,80)}
-
-myHandPositions6 = {'1a': (870,43,40,80), '1b': (964,43,40,80),
-                   '2a': (1378,138,40,80), '2b': (1476,139,40,80),
-                   '3a': (1426,458,40,80), '3b': (1521,455,40,80),
-                   '4a': (866,642,40,80), '4b': (963,645,40,80),
-                   '5a': (306,456,40,80), '5b': (404,457,40,80),
-                   '6a': (353,138,40,80), '6b': (448,138,40,80)}
-
-myHandPositions2 = {'1a': (870,43,40,80), '1b': (964,43,40,80),
-                   '2a': (866,642,40,80), '2b': (963,645,40,80)}
-
-dealerHandPositions = {'flop1':(713,354,40,80),
-                       'flop2':(815,355,40,80),
-                       'flop3':(914,355,40,80),
-                       'turnCard':(1015,353,40,80),
-                       'riverCard':(1116,353,40,80)}
-
-seatPositions9 = {'1': (1215,84,80,35), 
-                  '2': (1483,184,80,35),
-                  '3': (1545,384,80,35),
-                  '4': (1397,593,80,35),
-                  '5': (874,683,80,35),
-                  '6': (446,593,80,35),
-                  '7': (302,383,80,35),
-                  '8': (361,179,80,35),
-                  '9': (633,83,80,35)}
-
-seatPositions6 = {'1': (870,79,80,35),
-                  '2': (1480,179,80,35),
-                  '3': (1526,496,80,35),
-                  '4': (968,681,80,35),
-                  '5': (314,496,80,35),
-                  '6': (361,182,80,35)}
-
-seatPositions2 = {'1': (870,79,80,35), 
-                  '2': (967,683,80,35)}
+MainDict = {'2myHandPositions9': {'1a': (1104,41,40,80), '1b': (1200,40,40,80),
+                                    '2a': (1377,141,40,80), '2b': (1474,140,40,80),
+                                    '3a': (1441,342,40,80), '3b': (1535,340,40,80),
+                                    '4a': (1292,545,40,80), '4b': (1389,549,40,80),
+                                    '5a': (864,640,40,80), '5b': (962,640,40,80),
+                                    '6a': (436,548,40,80), '6b': (534,548,40,80),
+                                    '7a': (297,342,40,80), '7b': (388,339,40,80),
+                                    '8a': (351,137,40,80), '8b': (447,140,40,80),
+                                    '9a': (627,43,40,80), '9b': (722,41,40,80)},
+            '2myHandPositions6': {'1a': (870,43,40,80), '1b': (964,43,40,80),
+                                    '2a': (1378,138,40,80), '2b': (1476,139,40,80),
+                                    '3a': (1426,458,40,80), '3b': (1521,455,40,80),
+                                    '4a': (866,642,40,80), '4b': (963,645,40,80),
+                                    '5a': (306,456,40,80), '5b': (404,457,40,80),
+                                    '6a': (353,138,40,80), '6b': (448,138,40,80)},
+            '2myHandPositions2': {'1a': (870,43,40,80), '1b': (964,43,40,80),
+                                    '2a': (866,642,40,80), '2b': (963,645,40,80)},
+            '2dealerHandPositions': {'flop1':(713,354,40,80),
+                                    'flop2':(815,355,40,80),
+                                    'flop3':(914,355,40,80),
+                                    'turnCard':(1015,353,40,80),
+                                    'riverCard':(1116,353,40,80)},
+            '2seatPositions9': {'1': (1215,84,80,35), 
+                                '2': (1483,184,80,35),
+                                '3': (1545,384,80,35),
+                                '4': (1397,593,80,35),
+                                '5': (874,683,80,35),
+                                '6': (446,593,80,35),
+                                '7': (302,383,80,35),
+                                '8': (361,179,80,35),
+                                '9': (633,83,80,35)},
+            '2seatPositions6': {'1': (870,79,80,35),
+                                '2': (1480,179,80,35),
+                                '3': (1526,496,80,35),
+                                '4': (968,681,80,35),
+                                '5': (314,496,80,35),
+                                '6': (361,182,80,35)},
+            '2seatPositions2': {'1': (870,79,80,35), 
+                                '2': (967,683,80,35)},
+            '1myHandPositions9': {'1a': (877,46,35,77), '1b': (974,46,35,77),
+                                    '2a': (1147,144,35,77), '2b': (1242,144,35,77),
+                                    '3a': (1207,346,35,77), '3b': (1302,345,35,77),
+                                    '4a': (1065,551,35,77), '4b': (1157,553,35,77),
+                                    '5a': (638,645,35,77), '5b': (736,645,35,77),
+                                    '6a': (213,550,35,77), '6b': (308,553,35,77),
+                                    '7a': (70,344,35,77), '7b': (163,344,35,77),
+                                    '8a': (130,146,35,77), '8b': (224,144,35,77),
+                                    '9a': (398,47,35,77), '9b': (491,47,35,77)},
+            '1myHandPositions6': {'1a': (638,46,35,77), '1b': (734,45,35,77),
+                                    '2a': (1148,145,35,77), '2b': (1240,143,35,77),
+                                    '3a': (1194,458,35,77), '3b': (1289,459,35,77),
+                                    '4a': (638,643,35,77), '4b': (734,645,35,77),
+                                    '5a': (84,459,35,77), '5b': (179,456,35,77),
+                                    '6a': (130,144,35,77), '6b': (225,143,35,77)},
+            '1myHandPositions2': {'1a': (638,46,35,77), '1b': (734,45,35,77),
+                                    '4a': (638,643,35,77), '4b': (734,645,35,77)},
+            '1dealerHandPositions': {'flop1':(484,358,35,77),
+                                    'flop2':(585,360,35,77),
+                                    'flop3':(685,360,35,77),
+                                    'turnCard':(784,358,35,77),
+                                    'riverCard':(885,358,35,77)},
+            '1seatPositions9': {'1': (644,86,80,35),    
+                                '2': (1247,185,80,35),
+                                '3': (1308,385,80,35),
+                                '4': (1162,591,80,35),
+                                '5': (643,684,80,35),   
+                                '6': (219,591,80,35),
+                                '7': (75,384,80,35),
+                                '8': (137,187,80,35),
+                                '9': (403,87,80,35)},
+            '1seatPositions6': {'1': (645,88,80,35),
+                                '2': (1247,186,80,35),
+                                '3': (1294,500,80,35),
+                                '4': (739,684,80,35),
+                                '5': (89,500,80,35),
+                                '6': (135,186,80,35)},
+            '1seatPositions2': {'1': (643,86,80,35), 
+                                '2': (738,684,80,35)}}
 
 
 reset = driver.find_element_by_xpath('//*[@id="reset-poker-table-mobile"]')
@@ -318,18 +380,21 @@ flopClicked = 'no'
 turnClicked = 'no'
 riverClicked = 'no'
 if sizeOfTable == '2':
-    myHandPositions = myHandPositions2
-    playerSeats = seatPositions2
+    myHandPositions = screenKey+'myHandPositions2'
+    playerSeats = MainDict[screenKey+'seatPositions2']  
 elif sizeOfTable == '6':
-    myHandPositions = myHandPositions6
-    playerSeats = seatPositions6
+    myHandPositions = screenKey+'myHandPositions6'
+    playerSeats = MainDict[screenKey+'seatPositions6']
 elif sizeOfTable == '9':
-    myHandPositions = myHandPositions9
-    playerSeats = seatPositions9    
+    myHandPositions = screenKey+'myHandPositions9'
+    playerSeats = MainDict[screenKey+'seatPositions9']   
 if mySeat in playerSeats:
     del playerSeats[mySeat]
+  
 myHand1 = mySeat+'a'
 myHand2 = mySeat+'b'
+
+dealerHandPositions = screenKey+'dealerHandPositions'
 
 try:
     while True: #main loop
@@ -357,14 +422,16 @@ try:
             players = int(sizeOfTable)-1
             playerAddRemove()
         lastPlayers = players
+
+        
         if myCard1 is None:
-            myCard1 = findCard(myHandPositions, myHand1)
+            myCard1 = findCard(MainDict[myHandPositions], myHand1)
             logging.info('My 1st card is '+str(myCard1))
         if myCard2 is None:
-            myCard2 = findCard(myHandPositions, myHand2)
+            myCard2 = findCard(MainDict[myHandPositions], myHand2)
             logging.info('My 2nd card is '+str(myCard2))
         elif myCard1 is None and myCard2 is not None:
-            myCard1 = findCard(myHandPositions, myHand1)
+            myCard1 = findCard(MainDict[myHandPositions], myHand1)
             logging.info('2nd Pass: My 1st card is '+str(myCard1))
         if myCardsClicked == 'no':
             if myCard1 is not None and myCard2 is not None:
@@ -373,31 +440,31 @@ try:
                 driver.execute_script("arguments[0].scrollIntoView(true);", tableView)
         logging.info('myCardsClicked = '+myCardsClicked)
         if firstFlop is None:
-            firstFlop = findCard(dealerHandPositions, 'flop1')
+            firstFlop = findCard(MainDict[dealerHandPositions], 'flop1')
             logging.info('1st Flop is '+str(firstFlop))
         if secondFlop is None:
-            secondFlop = findCard(dealerHandPositions, 'flop2')
+            secondFlop = findCard(MainDict[dealerHandPositions], 'flop2')
             logging.info('2nd Flop is '+str(secondFlop))
         if thirdFlop is None:
-            thirdFlop = findCard(dealerHandPositions, 'flop3')
+            thirdFlop = findCard(MainDict[dealerHandPositions], 'flop3')
             logging.info('3rd Flop is '+str(thirdFlop))
         if turnCard is None:
-            turnCard = findCard(dealerHandPositions, 'turnCard')
+            turnCard = findCard(MainDict[dealerHandPositions], 'turnCard')
             logging.info('Turn Card is '+str(turnCard))
         if riverCard is None:
-            riverCard = findCard(dealerHandPositions, 'riverCard')
+            riverCard = findCard(MainDict[dealerHandPositions], 'riverCard')
             logging.info('River Card is '+str(riverCard))
         if firstFlop is None and secondFlop is not None:
-            firstFlop = findCard(dealerHandPositions, 'flop1')
+            firstFlop = findCard(MainDict[dealerHandPositions], 'flop1')
             logging.info('2nd Pass: 1st Flop is '+str(firstFlop))
         if secondFlop is None and thirdFlop is not None:
-            secondFlop = findCard(dealerHandPositions, 'flop2')
+            secondFlop = findCard(MainDict[dealerHandPositions], 'flop2')
             logging.info('2nd Pass: 2nd Flop is '+str(secondFlop))
         if thirdFlop is None and turnCard is not None:
-            thirdFlop = findCard(dealerHandPositions, 'flop3')
+            thirdFlop = findCard(MainDict[dealerHandPositions], 'flop3')
             logging.info('2nd Pass: 3rd Flop is '+str(thirdFlop))
         if turnCard is None and riverCard is not None:
-            turnCard = findCard(dealerHandPositions, 'turnCard')
+            turnCard = findCard(MainDict[dealerHandPositions], 'turnCard')
             logging.info('2nd Pass: turnCard is '+str(turnCard))
         if flopClicked == 'no':
             if firstFlop is not None and secondFlop is not None and thirdFlop is not None:
